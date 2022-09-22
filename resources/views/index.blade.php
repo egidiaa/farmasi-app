0ce3e0aa9d913984c0c51a9a244afaabbe930e62
egidiaa <112837582+egidiaa@users.noreply.github.com>
2022-09-22 12:25:36 +0700

    2

---
+++ resources/views/index.blade.php
@@ -0,0 +1,343 @@
+<!DOCTYPE html>
+
+<!--
+ // WEBSITE: https://themefisher.com
+ // TWITTER: https://twitter.com/themefisher
+ // FACEBOOK: https://www.facebook.com/themefisher
+ // GITHUB: https://github.com/themefisher/
+-->
+
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  ================================================== -->
+  <meta charset="utf-8">
+  <title>Novena- Health Care &amp; Medical template</title>
+
+  <!-- Mobile Specific Metas
+  ================================================== -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="Health Care Medical Html5 Template">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+  <meta name="author" content="Themefisher">
+  <meta name="generator" content="Themefisher Novena HTML Template v1.0">
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" type="image/x-icon" href="asset/logo.png" />
+
+  <!-- 
+  Essential stylesheets
+  =====================================-->
+  <link rel="stylesheet" href="plugins/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="plugins/icofont/icofont.min.css">
+  <link rel="stylesheet" href="plugins/slick-carousel/slick/slick.css">
+  <link rel="stylesheet" href="plugins/slick-carousel/slick/slick-theme.css">
+
+  <!-- Main Stylesheet -->
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body id="top">
+
+<header>
+	<div class="header-top-bar">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-6">
+					<ul class="top-bar-info list-inline-item pl-0 mb-0">
+						<li class="list-inline-item"><a href="mailto:support@gmail.com"><i class="icofont-support-faq mr-2"></i>Kelompok 14 B2</a></li>
+						<li class="list-inline-item"><i class="icofont-location-pin mr-2"></i>Surabaya, Indonesia</li>
+					</ul>
+				</div>
+				<div class="col-lg-6">
+					<div class="text-lg-right top-right-bar mt-2 mt-lg-0">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<nav class="navbar navbar-expand-lg navigation" id="navbar">
+		<div class="container">
+			<a class="navbar-brand" href="index.html">
+				<img src="asset/logo.png" alt="" class="img-fluid">
+			</a>
+			<h4 class="mb-3">NE PHARMACY</h4>
+
+			<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarmain"
+				aria-controls="navbarmain" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="icofont-navigation-menu"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarmain">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item active"><a class="nav-link" href="index.html">Beranda</a></li>
+					<li class="nav-item"><a class="nav-link" href="about.html">Tentang Kami</a></li>
+					<li class="nav-item"><a class="nav-link" href="service.html">Konsultasi</a></li>
+
+					
+					<li class="nav-item"><a class="nav-link" href="contact.html">Kontak Kami</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</header>
+
+
+<!-- Slider Start -->
+<section class="banner">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-md-12 col-xl-7">
+				<div class="block">
+					<div class="divider mb-3"></div>
+					<span class="text-uppercase text-sm letter-spacing ">Solusi Cepat Pembelian Obat Anda!</span>
+					<h1 class="mb-3 mt-3">Sistem Informasi Pelayanan Farmasi</h1>
+					
+					<p class="mb-4 pr-5">Sebuah layanan alternatif yang akan membantu anda dalam membeli obat secara instan dan sesuai dengan kebutuhan anda. Terdapat layanan konsultasi dengan dokter ahli untuk keluhan anda .</p>
+					<div class="btn-container ">
+						<a href="appoinment.html" target="_blank" class="btn btn-main-2 btn-icon btn-round-full">DAFTAR SEKARANG!!<i class="icofont-simple-right ml-2  "></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="features">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="feature-block d-lg-flex">
+
+				
+					<div class="feature-item mb-5 mb-lg-0">
+						<div class="feature-icon mb-4">
+							<i class="icofont-ui-clock"></i>
+						</div>
+						
+						<h4 class="mb-3">Jam Kerja</h4>
+						<ul class="w-hours list-unstyled">
+							<li class="d-flex justify-content-between">Senin - Rabu : <span>8:00 - 17:00</span></li>
+							<li class="d-flex justify-content-between">Kamis - jumat : <span>9:00 - 17:00</span></li>
+							<li class="d-flex justify-content-between">Sabtu - Minggu : <span>10:00 - 17:00</span></li>
+						</ul>
+					</div>
+				
+					<div class="feature-item mb-5 mb-lg-0">
+						<div class="feature-icon mb-4">
+							<i class="icofont-support"></i>
+						</div>
+						<span>Nomor Kontak</span>
+						<h4 class="mb-3">1-800-700-6200</h4>
+						<p>Kontak yang dapat dihubungi dalam keadaan darurat .</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="section about">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-lg-4 col-sm-6">
+				<div class="about-img">
+					<img src="asset/foto1.jfif" alt="" class="img-fluid">
+					<img src="asset/foto2.jfif" alt="" class="img-fluid mt-4">
+				</div>
+			</div>
+			<div class="col-lg-4 col-sm-6">
+				<div class="about-img mt-4 mt-lg-0">
+					<img src="asset/foto3.jpg" alt="" class="img-fluid">
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="about-content pl-4 mt-4 mt-lg-0">
+					<h2 class="title-color"> Untuk kesehatan & <br> Kenyamanan anda</h2>
+					<p class="mt-4 mb-5"> Kami siap melayani anda <br> Dapatkan informasi kesehatan, konsultasi dengan dokter, belanja vitamin & obat. #JagaSehatmu.</p>
+
+					<a href="service.html" class="btn btn-main-2 btn-round-full btn-icon">Konsultasikan sekarang juga!<i class="icofont-simple-right ml-3"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="section service gray-bg">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-lg-7 text-center">
+				<div class="section-title">
+					<h2>Pelayanan Kami</h2>
+					<div class="divider mx-auto my-4"></div>
+					<p>Cari Tahu apa yang Anda butuhkan di bawah ini.</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-lg-4 col-md-6 col-sm-6">
+				<div class="service-item mb-4">
+					<div class="icon d-flex align-items-center">
+						<i class="fa-thin fa-stethoscope"></i>
+						<h4 class="mt-3 mb-3">Konsultasi</h4>
+					</div>
+
+					<div class="content">
+						<p class="mb-4">Konsultasikan kepada Dokter terkait gejala-gejala yang anda rasakan.</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-6 col-sm-6">
+				<div class="service-item mb-4">
+					<div class="icon d-flex align-items-center">
+						<i class="icofont-heart-beat-alt text-lg"></i>
+						<h4 class="mt-3 mb-3">Vitamin dan Antibiotik</h4>
+					</div>
+					<div class="content">
+						<p class="mb-4"> Kuatkan stamina anda dan perketat daya tahan tubuh.</p>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-lg-4 col-md-6 col-sm-6">
+				<div class="service-item mb-4">
+					<div class="icon d-flex align-items-center">
+						<i class="icofont-tooth text-lg"></i>
+						<h4 class="mt-3 mb-3">Perawatan Luka</h4>
+					</div>
+					<div class="content">
+						<p class="mb-4">Rawat Luka anda dengan segera agar tidak terinfeksi.</p>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="col-lg-4 col-md-6 col-sm-6">
+				<div class="service-item mb-4">
+					<div class="icon d-flex align-items-center">
+						<i class="icofont-crutch text-lg"></i>
+						<h4 class="mt-3 mb-3">Obat General</h4>
+					</div>
+
+					<div class="content">
+						<p class="mb-4">Temukan Obat untuk sakit yang anda rasakan.</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-6 col-sm-6">
+				<div class="service-item mb-4">
+					<div class="icon d-flex align-items-center">
+						<i class="icofont-brain-alt text-lg"></i>
+						<h4 class="mt-3 mb-3">Obat Khusus</h4>
+					</div>
+					<div class="content">
+						<p class="mb-4">Temukan obat khusus untuk sakit yang anda rasakan.</p>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-lg-4 col-md-6 col-sm-6">
+				<div class="service-item mb-4">
+					<div class="icon d-flex align-items-center">
+						<i class="icofont-dna-alt-1 text-lg"></i>
+						<h4 class="mt-3 mb-3">Skincare</h4>
+					</div>
+					<div class="content">
+						<p class="mb-4">Rawat diri anda dengan sepenuh hati.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="section appoinment">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-lg-6 ">
+				<div class="appoinment-content">
+					<img src="images/about/img-3.jpg" alt="" class="img-fluid">
+					<div class="emergency">
+						<h2 class="text-lg"><i class="icofont-phone-circle text-lg"></i>+23 345 67980</h2>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-6 col-md-10 ">
+				<div class="appoinment-wrap mt-5 mt-lg-0">
+					<h2 class="mb-2 title-color">Layanan Konsultasi</h2>
+					<p class="mb-4">Konsultasikan kesehatan anda sekarang juga dibawah ini.</p>
+					     <form id="#" class="appoinment-form" method="post" action="#">
+                    <div class="row">
+                        
+                     
+
+                         <div class="col-lg-6">
+                            <div class="form-group">
+                                <input name="date" id="date" type="text" class="form-control" placeholder="dd/mm/yyyy">
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <input name="time" id="time" type="text" class="form-control" placeholder="Jam">
+                            </div>
+                        </div>
+                         <div class="col-lg-6">
+                            <div class="form-group">
+                                <input name="name" id="name" type="text" class="form-control" placeholder="Nama Lengkap">
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <input name="phone" id="phone" type="Number" class="form-control" placeholder="No Telepon">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group-2 mb-4">
+                        <textarea name="message" id="message" class="form-control" rows="6" placeholder="Pesan"></textarea>
+                    </div>
+
+                    <a class="btn btn-main btn-round-full" href="appoinment.html" >Kirim <i class="icofont-simple-right ml-2  "></i></a>
+                </form>
+            </div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="section testimonial-2 gray-bg">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-lg-7">
+				<div class="section-title text-center">
+					<h2>NE PHARMACY</h2>
+					<div class="divider mx-auto my-4"></div>
+					<p>Kami siap melayani kesehatan anda dengan sekali klik, apapun masalah kesehatan anda dapat dikosultasikan kepada Dokter di Apotek kami.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	
+   
+
+    <!-- 
+    Essential Scripts
+    =====================================-->
+    <script src="plugins/jquery/jquery.js"></script>
+    <script src="plugins/bootstrap/bootstrap.min.js"></script>
+    <script src="plugins/slick-carousel/slick/slick.min.js"></script>
+    <script src="plugins/shuffle/shuffle.min.js"></script>
+
+    <!-- Google Map -->
+    <script src="https://goo.gl/maps/QF7n8DaYHEcQiwQ37"></script>
+    <script src="plugins/google-map/gmap.js"></script>
+    
+    <script src="js/script.js"></script>
+
+  </body>
+  </html>
---
+++ routes/web.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('/about');
+});
+
+Route::get('/appoinment', function () {
+    return view('/appoinment');
+});
+
+Route::get('/blog-sidebar', function () {
+    return view('/blog-sidebar');
+});
+
+Route::get('/blog-single', function () {
+    return view('/blog-single');
+});
+
+Route::get('/confirmation', function () {
+    return view('/confirmation');
+});
+
+Route::get('/contact', function () {
+    return view('/contact');
+});
+
+Route::get('/department-single', function () {
+    return view('/department-single');
+});
+
+Route::get('/department', function () {
+    return view('/department');
+});
+
+Route::get('/doctor-single', function () {
+    return view('/doctor-single');
+});
+
+Route::get('/doctor', function () {
+    return view('/doctor');
+});
+
+Route::get('/sevice', function () {
+    return view('/service');
+});
